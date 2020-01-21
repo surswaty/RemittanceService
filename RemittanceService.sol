0185df977ca56b10d6aff8d263fa469f6d7ed2c8
Shams Coding Blockchain <55982732+Shamsyum@users.noreply.github.com>
2020-01-21 11:39:15 +0500

    logic improvement in RemittanceService.sol

--- RemittanceService.sol
+++ RemittanceService.sol
@@ -1,24 +1,31 @@
 pragma solidity ^0.5.0;
 
 contract RemittanceService{
-    mapping (address => uint)private Agent; //stores secret number in agent address
-    mapping (uint => uint)private Mony; //stores amount associated with the secret number
+    struct RemittanceData{
+        address agent;
+        uint value;
+        uint secretNum;
+    }
+    mapping (uint => RemittanceData) secretData;
+   
     event RemittanceTransfered(address sender, uint amount, address agent);
     event AgentWithdrawDone(address agent, uint amount, uint Fees);
     
     function transferRemmittance(address _agent, uint _secretNum)public payable returns(bool){
         require(msg.value > 0);
-        emit RemittanceTransfered(msg.sender, msg.value, _agent);
-        Agent[_agent] = _secretNum;
-        Mony[_secretNum] = msg.value;
+        uint secretNum = _secretNum;
+        RemittanceData memory _secretNum = RemittanceData(_agent, msg.value, _secretNum);
+        secretData[secretNum] = _secretNum;
+        return true;
     }
     
     function agentWithdraw(uint _secretNum, uint _amount)public returns(bool){
-        require(Agent[msg.sender] == _secretNum, "Not the agent");
-        require(Mony[_secretNum] <= _amount, "Insufficient Balance");
-        uint agentFee = (_amount * 1)/100; //1% agent fee
+       require(secretData[_secretNum].agent == msg.sender, "Not the agent");
+       require(secretData[_secretNum].value <= _amount, "Insufficient Balance");
+        uint agentFee = (_amount * 1)/100; // 1% agent fee
         emit AgentWithdrawDone(msg.sender,_amount, agentFee);
         msg.sender.transfer(_amount);
-        Mony[_secretNum] -= (_amount + agentFee);
+        secretData[_secretNum].value -= (_amount + agentFee);
+        return true;
     }
 }
