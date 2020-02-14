a5c099e57831ea638b0d670a5d89e187e9de3265
Shams Coding Blockchain <55982732+Shamsyum@users.noreply.github.com>
2020-02-14 07:05:46 +0500

    Some logic and syntax improvement

--- RemittanceService.sol
+++ RemittanceService.sol
@@ -6,7 +6,7 @@ contract RemittanceService{
         uint value;
         bytes32 secretHash;
     }
-    mapping (bytes32 => RemittanceData) secretData;
+    mapping (bytes32 => RemittanceData)public secretData;
    
     event RemittanceTransfered(address sender, uint amount, address agent);
     event AgentWithdrawDone(address agent, uint amount);
@@ -16,20 +16,23 @@ contract RemittanceService{
     }
     
     function transferRemmittance(address _agent, bytes32 _hash) public payable returns(bool){
-        require(msg.value > 0);
+        require(msg.value > 0, "Sends ether to remit.");
+        require(_hash != bytes32(0));
+        if(secretData[_hash].secretHash == _hash){
+            secretData[_hash].value += msg.value;
+        }
         RemittanceData memory myRem = RemittanceData(_agent, msg.value, _hash);
         secretData[_hash] = myRem;
         return true;
     }
     
-    function agentWithdraw(string memory _secretSrting, uint _amount)public returns(bool){
-        bytes32 secretHash = keccak256(abi.encodePacked(_secretSrting));
-        require(_amount > 0);
+    function agentWithdraw(string memory _secretSrting)public returns(bool){
+        bytes32 secretHash = hashIt(_secretSrting);
         require(secretData[secretHash].agent == msg.sender, "Not the agent");
-        require(_amount <= secretData[secretHash].value, "Insufficient Balance");
-        emit AgentWithdrawDone(msg.sender, _amount);
-        msg.sender.transfer(_amount);
-        secretData[secretHash].value -= _amount;
+        require(secretData[secretHash].value <= 0, "Have no Funds");
+        emit AgentWithdrawDone(msg.sender, secretData[secretHash].value);
+        msg.sender.transfer(secretData[secretHash].value);
+        delete secretData[secretHash];
         return true;
     }
 }
